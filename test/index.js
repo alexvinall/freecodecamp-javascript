df88e25a7fea888366bb16f5de1e08214a93a8a2
Alex Vinall <alexvinall@users.noreply.github.com>
2019-12-11 14:48:02 -0500

    Add telephone number validator

--- test/index.js
+++ test/index.js
@@ -2,6 +2,7 @@ let assert = require("assert");
 let palindrome = require("../src/projects/palindrome-checker.js");
 let convertToRoman = require("../src/projects/roman-numeral-converter.js");
 let rot13 = require("../src/projects/ceasars-cipher.js");
+let telephoneCheck = require("../src/projects/telephone-number-validator.js");
 
 // Test palindrome checker
 describe("Palindrome checker", function() {
@@ -55,5 +56,39 @@ describe("Caesar's cipher", function() {
     assert(rot13("SERR CVMMN!") === "FREE PIZZA!");
     assert(rot13("SERR YBIR?") === "FREE LOVE?");
     assert(rot13("GUR DHVPX OEBJA SBK WHZCF BIRE GUR YNML QBT.") === "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.");
+  });
+});
+
+// Test telephone number validator
+describe("Telephone number validator", function() {
+  it("Correctly validates telephone numbers", function() {
+    assert(!telephoneCheck());
+    assert(telephoneCheck("555-555-5555"));
+    assert(telephoneCheck("1 555-555-5555"));
+    assert(telephoneCheck("1 (555) 555-5555"));
+    assert(telephoneCheck("5555555555"));
+    assert(telephoneCheck("555-555-5555"));
+    assert(telephoneCheck("(555)555-5555"));
+    assert(telephoneCheck("1(555)555-5555"));
+    assert(!telephoneCheck("555-5555"));
+    assert(!telephoneCheck("5555555"));
+    assert(!telephoneCheck("1 555)555-5555"));
+    assert(telephoneCheck("1 555 555 5555"));
+    assert(telephoneCheck("1 456 789 4444"));
+    assert(!telephoneCheck("123**&!!asdf#"));
+    assert(!telephoneCheck("55555555"));
+    assert(!telephoneCheck("(6054756961)"));
+    assert(!telephoneCheck("2 (757) 622-7382"));
+    assert(!telephoneCheck("0 (757) 622-7382"));
+    assert(!telephoneCheck("-1 (757) 622-7382"));
+    assert(!telephoneCheck("2 757 622-7382"));
+    assert(!telephoneCheck("10 (757) 622-7382"));
+    assert(!telephoneCheck("27576227382"));
+    assert(!telephoneCheck("(275)76227382"));
+    assert(!telephoneCheck("2(757)6227382"));
+    assert(!telephoneCheck("2(757)622-7382"));
+    assert(!telephoneCheck("555)-555-5555"));
+    assert(!telephoneCheck("(555-555-5555"));
+    assert(!telephoneCheck("(555)5(55?)-5555"));
   });
 });
